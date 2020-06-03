2fbc3cfaf05c19daa03fc2ca6b3b3f607da5de13
denisEremeev22 <denisEremeev22@gmail.com>
2020-06-03 16:07:00 +0700

    Added page routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,28 @@ import { NgModule } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { SelectorComponent } from './selector/selector.component';
+import { LessonComponent } from './lesson/lesson.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ForStudentsComponent } from './for-students/for-students.component';
+
 import { MomentPipe } from './shared/moment.pipe';
 import { WeekPipe } from './shared/moment.weekPipe';
-import { LessonComponent } from './lesson/lesson.component';
 
 registerLocaleData(localeRu, 'ru');
+
+const appRouts: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'for-students', component: ForStudentsComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   declarations: [
@@ -21,8 +34,11 @@ registerLocaleData(localeRu, 'ru');
     MomentPipe,
     WeekPipe,
     LessonComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ForStudentsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRouts)],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/for-students/for-students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-for-students',
+  templateUrl: './for-students.component.html',
+  styleUrls: ['./for-students.component.scss']
+})
+export class ForStudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
